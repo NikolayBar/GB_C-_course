e2712e41feb106cc174bee4756cdecf6955890ab
Nikolay.B <nikolbar@yandex.ru>
2022-09-16 18:36:27 +0300

    add code in HW_11 ver1

--- sem01/HW_11/Program.cs
+++ sem01/HW_11/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num01=7;
+int num02=14;
+
+if (num01 != num02)
+{
+    if(num01>num02)
+    {
+        Console.WriteLine(num01+" больше "+num02);
+    }
+
+    else
+    {
+        Console.WriteLine(num02+" больше "+num01);
+    }
+
+}
+else
+{
+    Console.WriteLine(num01+" равно "+num02);
+}
