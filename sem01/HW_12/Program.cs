a70c6a3b3f8d29c932605f134c3e509c012af7e0
Nikolay.B <nikolbar@yandex.ru>
2022-09-16 20:47:27 +0300

    add code HW_12

--- sem01/HW_12/Program.cs
+++ sem01/HW_12/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num01=22;
+int num02=3;
+int num03=9;
+int max=0;
+
+if (num01>num02)
+{
+    max=num01;
+}
+else
+{
+    max=num02;
+}
+if (num03>max)
+{
+    max=num03;
+}
+Console.WriteLine("максимальное значение: "+max);
