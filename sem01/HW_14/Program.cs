21821c51f9bbcaf7e85cfbf38b3a07163cb71cce
Nikolay.B <nikolbar@yandex.ru>
2022-09-16 21:36:56 +0300

    добавил коменты в HW_1,HW_2,HW_4

---
+++ sem01/HW_14/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4
+//Программа на вход принимает число (num01), а на выходе показывает все чётные числа от 1 до num01.
+
+int num01=11;
+int curNum=1;
+
+Console.Clear();
+
+while(curNum<=num01)
+{
+    if(curNum%2 == 0)
+    {
+        Console.Write(curNum+" ");
+    }
+    curNum++;
+}
