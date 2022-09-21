34a64e3d901aa09d890dca9b4764382af893c663
Nikolay.B <nikolbar@yandex.ru>
2022-09-21 22:10:45 +0300

    Задача 10 вер2

--- sem02/HW_10/Program.cs
+++ sem02/HW_10/Program.cs
@@ -1,5 +1,10 @@
 ﻿//программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int A=918;
+//Console.Clear();
+//Console.WriteLine("Ввести трехзначное число :");
+//int A =Convert.ToInt32(Console.ReadLine());
+
+int A=728;
+
 Console.WriteLine((A/10)%10);
 
 
