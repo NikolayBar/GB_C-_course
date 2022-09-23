aaa4ede8da1667ccc8fd9f49f6d9d480b064b48a
Nikolay.B <nikolbar@yandex.ru>
2022-09-23 18:47:22 +0300

    Добавил решение HW_228

--- sem02/HW_228/Program.cs
+++ sem02/HW_228/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 228: Напишите программу, которая принимает 
+//на вход семь чисел, и находит их среднее арифметическое
+Console.Clear();
+int size=7;
+
+int[] Arr01 = new int[size];
+double sum =0;
+for(int i=0; i<size;i++)
+{
+    Arr01[i]= new Random().Next(1,70);
+    Console.Write(Arr01[i]+" ");
+    sum = sum + Arr01[i];
+}
+Console.Write("\n Среднее арифметическое:" + sum/Arr01.Length);
