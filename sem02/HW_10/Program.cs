d72315fbca8da26db71976a9da0257893ed68d16
Nikolay.B <nikolbar@yandex.ru>
2022-09-23 17:20:30 +0300

    добавлено решение в HW_13

--- sem02/HW_10/Program.cs
+++ sem02/HW_10/Program.cs
@@ -1,7 +1,6 @@
 ﻿//программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-//Console.Clear();
-//Console.WriteLine("Ввести трехзначное число :");
-//int A =Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
 
 int A=728;
 
--- sem02/HW_13/Program.cs
+++ sem02/HW_13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+int initNumber=78;
+string StrNumber=initNumber.ToString();
+
+if (StrNumber.Length<3)
+{
+    Console.Write("в числе "+ StrNumber + " меньше трех цифр.");
+}
+else
+{
+    Console.Write("в числе "+ StrNumber + " третья цифра слева - "+ StrNumber.Substring(2,1));
+}
+
+
+
+
+
