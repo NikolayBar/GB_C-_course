cefd57f4a84df010868a555681432e9843813b50
Nikolay.B <nikolbar@yandex.ru>
2022-09-23 23:08:11 +0300

    добавлена задача HW_21

---
+++ sem03/HW_21/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите метод, который принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+
+
+double DistansAB(int[] A, int[] B)
+{
+double sum=0;
+for(int i=0; i<3; i++)
+{
+    sum = sum + Math.Pow((B[i]-A[i]),2);
+}
+return Math.Sqrt(sum);
+}
+
+int[] A = new int[]{7,-5,0};
+int[] B = new int[]{1,-1,9};
+
+Console.Clear();
+Console.WriteLine(DistansAB(A,B).ToString("#.00"));
