7857e351c584cbfabde3b170e694a96e6cb0b244
Nikolay.B <nikolbar@yandex.ru>
2022-09-24 20:50:50 +0300

    добавлено решение HW_19

--- sem03/HW_19/Program.cs
+++ sem03/HW_19/Program.cs
@@ -1,3 +1,26 @@
 ﻿//Задача 19
 //Напишите метод, который принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+string PlndrTest(int InputDig)
+{
+    string Text = Convert.ToString(InputDig);
+    string Result="не является палиндромом";
+    string Txet="";
+    foreach(char var in Text)
+    {
+        Txet= var + Txet;
+    }
+
+    if (Text == Txet) 
+    {
+        Result = "является палиндромом";
+    }
+    return Result;
+}
+
+Console.Clear();
+
+int inpDigit=12421;//число по условию задачи онли.
+
+Console.WriteLine("число "+ inpDigit +" " + PlndrTest(inpDigit));
+
