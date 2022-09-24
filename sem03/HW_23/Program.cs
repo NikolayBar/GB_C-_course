2a8553dc804f95fe9a35f321a1888bb30c52b245
Nikolay.B <nikolbar@yandex.ru>
2022-09-24 22:09:27 +0300

    Добавлено решение HW_23

--- sem03/HW_23/Program.cs
+++ sem03/HW_23/Program.cs
@@ -1,2 +1,17 @@
 ﻿//Задача 23
 //Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubeTab(int N)
+{
+    for(int i=1; i<=N; i++)
+    {
+        Console.WriteLine(i+"^3 = " + Math.Pow(i,3));
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число до которого \nнужно вычислить таблицу кубов: ");
+
+int Digit = Convert.ToInt32(Console.ReadLine());
+
+CubeTab(Digit);
