a07e7b017ac9787351a707e0519cd46b3ed60ff8
Nikolay.B <nikolbar@yandex.ru>
2022-09-27 22:01:52 +0300

    отредактировано решение HW_21

--- sem03/HW_21/Program.cs
+++ sem03/HW_21/Program.cs
@@ -1,19 +1,39 @@
 ﻿//Напишите метод, который принимает на вход координаты двух точек 
 //и находит расстояние между ними в 3D пространстве.
 
+double[] InputCoord()
+{
+    string[] nameCoord= {"X","Y","Z"};// наменование координат
+    string tmpInput = string.Empty;// временное строковое значение координаты
+    double[] meanCoord= new double[3];// массив для координат точек
+    for(int i=0;i<3;i++)
+    {
+        Console.Write("Координата {0}:", nameCoord[i]);
+        tmpInput=Console.ReadLine()!;
+        // добавить проверку введенного значения
+        meanCoord[i]=Convert.ToDouble(tmpInput);
+    }
 
-double DistansAB(int[] A, int[] B)
+    return meanCoord;
+}
+
+double DistansXYZ(double[] dotA, double[] dotB)
 {
 double sum=0;
 for(int i=0; i<3; i++)
 {
-    sum = sum + Math.Pow((B[i]-A[i]),2);
+    sum = sum + Math.Pow((dotB[i]-dotA[i]),2);
 }
 return Math.Sqrt(sum);
 }
 
-int[] startDot = new int[]{7,-5,0};
-int[] endDot = new int[]{1,-1,9};
-
 Console.Clear();
-Console.WriteLine(DistansAB(startDot,endDot).ToString("#.00"));
+Console.WriteLine("Положение начальной точки:");
+double[] startDot=InputCoord();
+
+Console.WriteLine("Положение конечной точки:");
+double[] endDot=InputCoord();
+
+
+Console.Write("Расстояние между точками = ");
+Console.Write(DistansXYZ(startDot,endDot).ToString("#.00"));
