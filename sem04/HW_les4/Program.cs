db3190f6af5963436920fd97c01d247361353165
Nikolay.B <nikolbar@yandex.ru>
2022-10-01 04:38:31 +0300

    решение ver1 HW_les4

--- sem04/HW_les4/Program.cs
+++ sem04/HW_les4/Program.cs
@@ -17,18 +17,36 @@
 11-12
 */
 
-int[] ListOfIncoming = new int[5]{5,6,7,9,8}; //список входящих
-int[] ListOfOutput =   new int[5]{4,3,2,8,1}; // список выходящих
+//Заполнение списка пришло/ушло охранником
+int[] ListOfIncoming = new int[5]; //список входящих
+int[] ListOfOutput =   new int[5]; // список выходящих
+
+int RndPeople(int maxPeople)//случайное число пришел/ушел
+{
+    int result = new Random().Next(1,maxPeople);
+    return result;
+}
+
+int oldVisitors = 0;// покупателей при открытии
+for (int i=0;i<5;i++) // заполняем списки пришел/ушел
+{
+    ListOfIncoming[i]= RndPeople(100-oldVisitors);// зашло покупателей
+    ListOfOutput[i]= RndPeople(ListOfIncoming[i] + oldVisitors);//вышло покупателей
+    oldVisitors=ListOfIncoming[i]-ListOfOutput[i];// осталось в магазине
+    if(oldVisitors<0) oldVisitors=0;// проверка на отрицательный результат
+}
+// список заполнен
 
 string[] WorkingTime = new string [5]{"9-10","10-11","11-12","12-13","13-14"};//рабочие часы
-int max=0;
+int max=0;// максимальное число покупателей 
 
-int[] PiopleCost()
+int[] NumberOfVisitorsPerHours()// число покупателей по часам
 {
     int[] PeopleInRoom = new int[5];
     for (int i=0; i<5;i++)
         {
-           PeopleInRoom[i]= ListOfIncoming[i]-ListOfOutput[i];
+           PeopleInRoom[i]= ListOfIncoming[i]-ListOfOutput[i];//разница вошедших/ушедших
+           if (i>0) PeopleInRoom[i]=PeopleInRoom[i]+PeopleInRoom[i-1];// остались с прошлого часа
         }
     return PeopleInRoom;
 }
@@ -48,9 +66,12 @@ int FindMax(int[] A)
     return maxIndex;
 }
 
-string MaxPeopleTime=WorkingTime[FindMax(PiopleCost())];
+string MaxPeopleTime=WorkingTime[FindMax(NumberOfVisitorsPerHours())];
 
 
 Console.Clear();
 Console.WriteLine("Наибольшее количество посетителей ({0} чел.) ", max);
 Console.WriteLine("находилось в магазине в период " + MaxPeopleTime + " час.");
+Console.WriteLine();
+Console.WriteLine(String.Join("|", ListOfIncoming)+ " - зашло покупателей");
+Console.WriteLine(String.Join("|", ListOfOutput) + " - вышло покупателей");
