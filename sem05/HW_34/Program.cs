c4b9f52010b881264d2aa6ac424e627190b27fdc
Nikolay.B <nikolbar@yandex.ru>
2022-10-05 02:24:04 +0300

    добавлено решение HW_36

--- sem05/HW_34/Program.cs
+++ sem05/HW_34/Program.cs
@@ -1,16 +1,13 @@
 ﻿/*
 Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
-Напишите программу, которая покажет количество чётных чисел в массиве. The number of even numbers
+Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
 
 int[] ArrThreeDigNum(int arrSize)// заполняем массив трехзначными числами
 {
     int[] array= new int[arrSize];
-    for(int i=0;i<arrSize;i++)
-    {
-        array[i]= new Random().Next(100,1000);
-    }
+    for(int i=0;i<arrSize;i++) { array[i]= new Random().Next(100,1000);}
     return array;
 }
 
@@ -18,10 +15,7 @@ double NumOfEvenNum(int[] Array)// считаем четные числа
 {
 int size = Array.Length;
 int res=0;
-for(int i=0; i<size;i++)
-{
-    if(Array[i]%2==0) res++;
-}
+for(int i=0; i<size;i++) { if(Array[i]%2==0) res++;}
 return res;
 }
 
--- sem05/HW_36/Program.cs
+++ sem05/HW_36/Program.cs
@@ -4,3 +4,20 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+
+int[] ArrRandomNumber(int arrSize, int valueMin, int valueMax)// заполняем массив случайными числами
+{
+    int[] array = new int[arrSize];
+    for (int i = 0; i < arrSize; i++) { array[i] = new Random().Next(valueMin, valueMax); }
+    return array;
+}
+
+int[] ArrRandom = ArrRandomNumber(6, -20, 20);
+int size = ArrRandom.Length;
+int sum = 0;
+for (int i = 1; i < size; i += 2) { sum += ArrRandom[i]; }
+
+Console.Clear();
+Console.Write("Массив : |");
+Console.Write(string.Join("|", ArrRandom));
+Console.Write($"|\nСумма элементов на нечётных позициях: {sum}\n ");
