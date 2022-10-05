8626864ff377d171dcf70a7950a5ad219050bfdb
Nikolay.B <nikolbar@yandex.ru>
2022-10-05 14:23:45 +0300

    Решено HW_38

--- sem05/HW_38/Program.cs
+++ sem05/HW_38/Program.cs
@@ -11,7 +11,35 @@ double[] ArrRandomNumber(int arrSize, int valueMin, int valueMax)// заполн
     return array;
 }
 
-double[] ArrMaterialNumber=ArrRandomNumber(5,0,80);
-Console.WriteLine(string.Join("|",ArrMaterialNumber));
+double FindMax(double[] ArrInput)//поиск максимального знач.
+{
+    int size = ArrInput.Length;
+    double maxValue = ArrInput[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (ArrInput[i] > maxValue) { maxValue = ArrInput[i]; }
+    }
+    return maxValue;
+}
 
-//int[] ArrMaterialNumber=ArrRandomNumber(5,0,100);
+double FindMin(double[] ArrInput)//поиск минимального знач.
+{
+    int size = ArrInput.Length;
+    double minValue = ArrInput[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (ArrInput[i] < minValue) { minValue = ArrInput[i]; }
+    }
+    return minValue;
+}
+
+
+double[] ArrMaterialNumber = ArrRandomNumber(5, 0, 80);
+
+double maxValue = FindMax(ArrMaterialNumber);
+double minValue = FindMin(ArrMaterialNumber);
+
+Console.Clear();
+Console.WriteLine(string.Join("|", ArrMaterialNumber));
+Console.WriteLine($"{maxValue} - {minValue} = {maxValue - minValue}");
+
