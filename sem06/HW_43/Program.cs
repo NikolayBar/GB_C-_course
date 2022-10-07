ec5f8266b2c6d99c64b8963c44d1b5d04935b375
Nikolay.B <nikolbar@yandex.ru>
2022-10-07 23:44:08 +0300

    Решено HW_43

--- sem06/HW_43/Program.cs
+++ sem06/HW_43/Program.cs
@@ -5,45 +5,104 @@ y = k2 * x + b2;
 
 x = (b1-b2)/(k1-k2)
 
-значения b1, k1, b2 и k2 задаются пользователем. 
+значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
-//Значения заданые пользователем
-double coordX;
-double coordY;
-double b1 = 2.0;
-double b2 = 4.0;
-double k1 = 5.0;
-double k2 = 9.0;
-
-double[,] introducedCoordinates = new double[2, 2]
+Console.Clear();
+Console.Write("Найти точку пересечения двух \nпрямых ");
+Console.WriteLine("заданых уравнениями:\n\ny = k1 * x + b1\ny = k2 * x + b2\n");
+bool СheckForNumbers(string testOnDigits)
 {
-    {b1,b2},
-    {k1,k2}
-};
-
-string nm=" b";
-for(int row=0;row<2;row++)
-{
-    for(int col=0;col<2;col++)
+    char firstSymb = Char.Parse(testOnDigits.Substring(0, 1));
+    if (firstSymb == '-' | firstSymb == '+')
     {
-        Console.Write($"{nm}{col+1} = {introducedCoordinates[row,col]}|");
-        
+        testOnDigits = testOnDigits.Remove(0, 1);
     }
-    nm=" k";
-    Console.Write("\n");
+    // кусок кода с какого-то сайта
+    foreach (var item in testOnDigits)
+    {
+        if (!char.IsDigit(item))
+        {
+            return false; //если хоть один символ не число- "ложь"
+        }
+    }
+    return true;
 }
+
+int? UserDigInput()
+{
+    string? stringInput = "";
+    bool inputType = true;
+
+    while (inputType)
+    {
+        Console.Write(">:");
+        stringInput = Console.ReadLine()!;
+        if (stringInput != "")
+        {
+            inputType = !СheckForNumbers(stringInput);//проверить число ли
+        }
+        else { return null; }// если пусто вернуть null
+
+    }
+
+    return Convert.ToInt32(stringInput);
+
+}
+
+double[,] userDataRequest()
+{
+    string[,] PosName = new string[2, 2]
+    {
+    {"b1","b2"},
+    {"k1","k2"}
+    }; 
+    
+    double[,] ArrUserData = new double[2, 2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            Console.Write($"{j+1}я прямая, значение {PosName[i, j]}");
+            ArrUserData[i, j] = Convert.ToDouble(UserDigInput());
+        }
+    }
+    return ArrUserData;
+}
+
+double[,] dataEnteredUser = userDataRequest();
+
+double coordX = 0.0;
+double coordY = 0.0;
+string Answer;
 
 void CalculateIntersectionPoint(double[,] inputCoord)
 {
-    double[] result = new double[2];
-    coordX=(inputCoord[0,1]-inputCoord[0,0])/(inputCoord[1,0]-inputCoord[1,1]);
-    coordY=(inputCoord[1,0]*coordX)+inputCoord[0,0];
+    if (inputCoord[1, 0] != inputCoord[1, 1])
+    {
+        coordX = (inputCoord[0, 1] - inputCoord[0, 0]) / (inputCoord[1, 0] - inputCoord[1, 1]);
+        coordY = (inputCoord[1, 0] * coordX) + inputCoord[0, 0];
+    }
+    if (inputCoord[1, 0] == inputCoord[1, 1])
+    {
+        if (inputCoord[0, 1] == inputCoord[0, 0])
+        {
+            Answer = "Прямые совпадают";
+        }
+        else
+        {
+            Answer = "Прямые паралельны";
+        }
+    }
+    else
+    {
+        Answer = $"Прямые пересекаются в точке X={coordX} / Y={coordY}).";
+    }
 }
 
-CalculateIntersectionPoint(introducedCoordinates);
-Console.Clear();
-Console.WriteLine($"\n k1*x + b1 = {introducedCoordinates[1,0]*coordX+introducedCoordinates[0,0]}");
-Console.WriteLine($"\n k2*x + b2 = {introducedCoordinates[1,1]*coordX+introducedCoordinates[0,1]}");
-Console.WriteLine($"точка пересечения в координатах х={coordX} , y={coordY}");
+CalculateIntersectionPoint(dataEnteredUser);
+
+Console.WriteLine();
+Console.WriteLine(Answer);
