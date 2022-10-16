619314dbf395e7124e80b7ba946063ea58dd08e2
Nikolay.B <nikolbar@yandex.ru>
2022-10-16 20:02:50 +0300

    измененен вывод в HW_les4

--- sem04/HW_les4/Program.cs
+++ sem04/HW_les4/Program.cs
@@ -19,63 +19,78 @@
 
 //Заполнение списка пришло/ушло охранником
 //Время работы магазина 5 часов с 9:00 до 14:00
-string[] WorkingTime = new string [5]{"9-10","10-11","11-12","12-13","13-14"};//рабочие часы
+string[] WorkingTime = new string[5] { "09-10", "10-11", "11-12", "12-13", "13-14" };//рабочие часы
 int[] ListOfIncoming = new int[5]; //список входящих
-int[] ListOfOutput =   new int[5]; // список выходящих
+int[] ListOfOutput = new int[5]; // список выходящих
+const int cellWidth = 5;
 
 int RndPeople(int maxPeople)//случайное число пришел/ушел
 {
-    
-   if(maxPeople<=0) maxPeople = 1;
-   int result = new Random().Next(0,maxPeople);
-    
+
+    if (maxPeople <= 0) maxPeople = 1;
+    int result = new Random().Next(0, maxPeople);
+
     return result;
 }
 
 int oldVisitors = 0;// покупателей при открытии
-for (int i=0;i<5;i++) // заполняем списки пришел/ушел
-{  
-    ListOfIncoming[i]= RndPeople(99-oldVisitors);// зашло покупателей
-    ListOfOutput[i]= RndPeople(ListOfIncoming[i]+oldVisitors);//вышло покупателей
-    oldVisitors=oldVisitors+(ListOfIncoming[i]-ListOfOutput[i]);//осталось в магазине
+for (int i = 0; i < 5; i++) // заполняем списки пришел/ушел
+{
+    ListOfIncoming[i] = RndPeople(99 - oldVisitors);// зашло покупателей
+    ListOfOutput[i] = RndPeople(ListOfIncoming[i] + oldVisitors);//вышло покупателей
+    oldVisitors = oldVisitors + (ListOfIncoming[i] - ListOfOutput[i]);//осталось в магазине
 }
 // список заполнен
 
 
-int max=0;// максимальное число покупателей 
+int max = 0;// максимальное число покупателей 
 
 int[] NumberOfVisitorsPerHours()// число покупателей по часам
 {
     int[] PeopleInRoom = new int[5];
-    for (int i=0; i<5;i++)
-        {
-           PeopleInRoom[i]= ListOfIncoming[i]-ListOfOutput[i];//разница вошедших/ушедших
-           if (i>0) PeopleInRoom[i]=PeopleInRoom[i]+PeopleInRoom[i-1];// остались с прошлого часа
-        }
+    for (int i = 0; i < 5; i++)
+    {
+        PeopleInRoom[i] = ListOfIncoming[i] - ListOfOutput[i];//разница вошедших/ушедших
+        if (i > 0) PeopleInRoom[i] = PeopleInRoom[i] + PeopleInRoom[i - 1];// остались с прошлого часа
+    }
     return PeopleInRoom;
 }
 
 int FindMax(int[] A)
 {
     max = A[0];
-    int maxIndex=0;
-    for(int i=0;i<5;i++)
+    int maxIndex = 0;
+    for (int i = 0; i < 5; i++)
     {
-        if (A[i]>max) 
+        if (A[i] > max)
         {
-            max=A[i];
-            maxIndex=i;
+            max = A[i];
+            maxIndex = i;
         }
     }
     return maxIndex;
 }
 
-string MaxPeopleTime=WorkingTime[FindMax(NumberOfVisitorsPerHours())];
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i],cellWidth}|");
+    }
+}
+
+string MaxPeopleTime = WorkingTime[FindMax(NumberOfVisitorsPerHours())];
 
 
 Console.Clear();
-Console.WriteLine("Наибольшее количество посетителей ({0} чел.) ", max);
-Console.WriteLine("находилось в магазине в период " + MaxPeopleTime + " час.");
+Console.ForegroundColor=ConsoleColor.DarkGray;
+Console.WriteLine($"Наибольшее количество посетителей ({max} чел.) ");
+Console.WriteLine($"находилось в магазине в период {MaxPeopleTime} час.");
 Console.WriteLine();
-Console.WriteLine(String.Join("|", ListOfIncoming)+ " - зашло покупателей");
-Console.WriteLine(String.Join("|", ListOfOutput) + " - вышло покупателей");
+Console.ForegroundColor=ConsoleColor.White;
+Console.WriteLine(String.Join("|",WorkingTime)+"|");
+PrintArray(ListOfIncoming);
+Console.Write(" - зашло покупателей\n");
+PrintArray(ListOfOutput);
+Console.Write(" - вышло покупателей\n");
+Console.ReadKey();
